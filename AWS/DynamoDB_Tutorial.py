cc4b1f538affa1cc92be5e2ba3f6bed72a248b91
Teslassian <dvoneschwege@gmail.com>
2021-01-17 18:33:32 +0200

    Completed the basic python AWS SDK operations

--- AWS/DynamoDB_Tutorial.py
+++ AWS/DynamoDB_Tutorial.py
@@ -3,6 +3,12 @@ from pprint import pprint
 import json
 import boto3
 from botocore.exceptions import ClientError
+from boto3.dynamodb.conditions import Key
+
+# To run the code using the DynamoDB web service, change the line
+# dynamodb = boto3.resource('dynamodb',endpoint_url="http://localhost:8000")
+# to
+# dynamodb = boto3.resource('dynamodb',region_name='us-east-1')
 
 def create_movie_table(dynamodb=None):
     if not dynamodb:
@@ -178,56 +184,97 @@ def query_movies(year, dynamodb=None):
     )
     return response['Items']
 
+def query_and_project_movies(year, title_range, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
+
+    table = dynamodb.Table('Movies')
+    print(f"Get year, title, genres, and lead actor")
+
+    # Expression attribute names can only reference items in the projection expression.
+    response = table.query(
+        ProjectionExpression="#yr, title, info.genres, info.actors[0]",
+        ExpressionAttributeNames={"#yr": "year"},
+        KeyConditionExpression=Key('year').eq(year) & Key('title').between(title_range[0], title_range[1])
+    )
+    return response['Items']
+
+def scan_movies(year_range, display_movies, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
+
+    table = dynamodb.Table('Movies')
+    scan_kwargs = {
+        'FilterExpression': Key('year').between(*year_range),
+        'ProjectionExpression': "#yr, title, info.rating",
+        'ExpressionAttributeNames': {"#yr": "year"}
+    }
+
+    done = False
+    start_key = None
+    while not done:
+        if start_key:
+            scan_kwargs['ExclusiveStartKey'] = start_key
+        response = table.scan(**scan_kwargs)
+        display_movies(response.get('Items', []))
+        start_key = response.get('LastEvaluatedKey', None)
+        done = start_key is None
+
+def delete_movie_table(dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
+
+    table = dynamodb.Table('Movies')
+    table.delete()
+
 if __name__ == '__main__':
 
-    # # Create the table
-    # input("Press enter to create the table")
-    # movie_table = create_movie_table()
-    # print("Table status: ", movie_table.table_status)
+    # Create the table
+    movie_table = create_movie_table()
+    print("Table status: ", movie_table.table_status)
 
-    # # Populate the table
-    # input("Press enter to populate the table")
-    # with open("moviedata.json") as json_file:
-    #     movie_list = json.load(json_file, parse_float=Decimal)
-    # load_movies(movie_list)
+    # Populate the table
+    with open("moviedata.json") as json_file:
+        movie_list = json.load(json_file, parse_float=Decimal)
+    load_movies(movie_list)
 
-    # # Add an item
-    # movie_resp = put_movie("The Big New Movie", 2015,
-    #                        "Nothing happens at all.", 0)
-    # print("Put movie succeeded:")
-    # pprint(movie_resp, sort_dicts=False)
+    # Add an item
+    movie_resp = put_movie("The Big New Movie", 2015,
+                           "Nothing happens at all.", 0)
+    print("Put movie succeeded:")
+    pprint(movie_resp, sort_dicts=False)
 
-    # # Read an item
-    # movie = get_movie("The Big New Movie", 2015,)
-    # if movie:
-    #     print("Get movie succeeded:")
-    #     pprint(movie, sort_dicts=False)
+    # Read an item
+    movie = get_movie("The Big New Movie", 2015,)
+    if movie:
+        print("Get movie succeeded:")
+        pprint(movie, sort_dicts=False)
 
-    # # Update an item
-    # update_response = update_movie(
-    #     "The Big New Movie", 2015, 5.5, "Everything happens all at once.",
-    #     ["Larry", "Moe", "Curly"])
-    # print("Update movie succeeded:")
-    # pprint(update_response, sort_dicts=False)
+    # Update an item
+    update_response = update_movie(
+        "The Big New Movie", 2015, 5.5, "Everything happens all at once.",
+        ["Larry", "Moe", "Curly"])
+    print("Update movie succeeded:")
+    pprint(update_response, sort_dicts=False)
 
-    # # Increment an Atomic Counter
-    # update_response = increase_rating("The Big New Movie", 2015, 1)
-    # print("Update movie succeeded:")
-    # pprint(update_response, sort_dicts=False)
+    # Increment an Atomic Counter
+    update_response = increase_rating("The Big New Movie", 2015, 1)
+    print("Update movie succeeded:")
+    pprint(update_response, sort_dicts=False)
 
-    # # Update an item (Conditionally)
-    # print("Attempting conditional update (expecting failure)...")
-    # update_response = remove_actors("The Big New Movie", 2015, 3)
-    # if update_response:
-    #     print("Update movie succeeded:")
-    #     pprint(update_response, sort_dicts=False)
+    # Update an item (Conditionally)
+    print("Attempting conditional update (expecting failure)...")
+    update_response = remove_actors("The Big New Movie", 2015, 3)
+    if update_response:
+        print("Update movie succeeded:")
+        pprint(update_response, sort_dicts=False)
 
-    # # Delete an item
-    # print("Attempting a conditional delete...")
-    # delete_response = delete_underrated_movie("The Big New Movie", 2015, 5)
-    # if delete_response:
-    #     print("Delete movie succeeded:")
-    #     pprint(delete_response, sort_dicts=False)
+    # Delete an item
+    print("Attempting a conditional delete...")
+    delete_response = delete_underrated_movie("The Big New Movie", 2015, 5)
+    if delete_response:
+        print("Delete movie succeeded:")
+        pprint(delete_response, sort_dicts=False)
 
     # Query items
     query_year = 2001
@@ -235,3 +282,26 @@ if __name__ == '__main__':
     movies = query_movies(query_year)
     for movie in movies:
         print(movie['year'], ":", movie['title'])
+
+    # Query items with filter
+    query_year = 1992
+    query_range = ('A', 'L')
+    print(f"Get movies from {query_year} with titles from "
+          f"{query_range[0]} to {query_range[1]}")
+    movies = query_and_project_movies(query_year, query_range)
+    for movie in movies:
+        print(f"\n{movie['year']} : {movie['title']}")
+        pprint(movie['info'])
+
+    # Scan the entire table, filter afterwards
+    def print_movies(movies):
+        for movie in movies:
+            print(f"\n{movie['year']} : {movie['title']}")
+            pprint(movie['info'])
+    query_range = (1950, 1959)
+    print(f"Scanning for movies released from {query_range[0]} to {query_range[1]}...")
+    scan_movies(query_range, print_movies)
+
+    # Delete the table
+    delete_movie_table()
+    print("Movies table deleted.")
