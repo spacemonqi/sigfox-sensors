7724ceaffd76d7114b58121c7417d00e766d37f1
Teslassian <dvoneschwege@gmail.com>
2021-01-20 17:49:17 +0200

    Created init.py to prevent the program from reinitializing twice

--- AWS/_Demo_Dash/demo.py
+++ AWS/_Demo_Dash/demo.py
@@ -28,56 +28,6 @@ import os
 # Visit http://127.0.0.1:8050/ in your web browser.
 
 #----------------------------------------------------------------------------------------------------------------------#
-def create_sigfox_table_AWS(dynamodb=None):
-    if not dynamodb:
-        if online:
-            dynamodb = boto3.resource('dynamodb',region_name='us-east-1')
-        else:
-            dynamodb = boto3.resource('dynamodb',endpoint_url="http://localhost:8000")
-
-    table = dynamodb.create_table(
-        TableName=tableName,
-        KeySchema=[
-            {
-                'AttributeName': 'deviceId',
-                'KeyType': 'HASH' # Partition Key
-            },
-            {
-                'AttributeName': 'timestamp',
-                'KeyType': 'RANGE' # Sort Key
-            }
-        ],
-        AttributeDefinitions=[
-            {
-                'AttributeName': 'deviceId',
-                'AttributeType': 'S'
-            },
-            {
-                'AttributeName': 'timestamp',
-                'AttributeType': 'N'
-            }
-        ],
-        ProvisionedThroughput={
-            'ReadCapacityUnits': 10,
-            'WriteCapacityUnits': 10
-        },
-        StreamSpecification={
-            'StreamEnabled': True,
-            'StreamViewType': 'NEW_AND_OLD_IMAGES'
-        }
-    )
-    return table
-
-def delete_sigfox_table_AWS(dynamodb=None):
-    if not dynamodb:
-        if online:
-            dynamodb = boto3.resource('dynamodb',region_name='us-east-1')
-        else:
-            dynamodb = boto3.resource('dynamodb',endpoint_url="http://localhost:8000")
-
-    table = dynamodb.Table(tableName)
-    table.delete()
-
 def scan_items_AWS(dynamodb=None):
     if not dynamodb:
         if online:
@@ -106,67 +56,14 @@ def query_and_project_items_AWS(deviceId, last_timestamp, dynamodb=None):
     )
     return response['Items']
 
-def put_item_AWS(deviceId, timestamp, data, deviceTypeId, seqNumber, time, dynamodb=None):
-    if not dynamodb:
-        if online:
-            dynamodb = boto3.resource('dynamodb',region_name='us-east-1')
-        else:
-            dynamodb = boto3.resource('dynamodb',endpoint_url="http://localhost:8000")
-
-    table = dynamodb.Table(tableName)
-    response = table.put_item(
-        Item={
-            'deviceId': deviceId,
-            'timestamp': timestamp,
-            'payload': {
-                'data': data,
-                'deviceTypeId': deviceTypeId,
-                'seqNumber' : seqNumber,
-                'time' : time
-            }
-        }
-    )
-    return response
-
-# def get_item_AWS(deviceId, timestamp, dynamodb=None):
-#     if not dynamodb:
-#         if online:
-#             dynamodb = boto3.resource('dynamodb',region_name='us-east-1')
-#         else:
-#             dynamodb = boto3.resource('dynamodb',endpoint_url="http://localhost:8000")
-#
-#     table = dynamodb.Table(tableName)
-#
-#     try:
-#         response = table.get_item(Key={'deviceId': deviceId, 'timestamp': timestamp})
-#     except ClientError as e:
-#         print(e.response['Error']['Message'])
-#     else:
-#         return response['Item']
-
 #----------------------------------------------------------------------------------------------------------------------#
 def now():
     return round(datetime.timestamp(datetime.now()))
 
-def populate_table():
-    deviceId = '12CAC94'
-    deviceTypeId = '5ff717c325643206e8d57c11'
-    seqNumber = 25
-    for x in range(num_items):
-        timestamp = now() + x - num_items
-        time = timestamp
-        data = round(1000*math.sin(0.1*x) * math.cos(x))
-        item_resp = put_item_AWS(deviceId, timestamp, data, deviceTypeId, seqNumber, time)
-        if (x % 10 == 0):
-            print("Put " + str(x) + " items")
-
-def create_dataframe():
+def scan_to_dataframe():
     columns = ['deviceId', 'timestamp', 'data', 'deviceTypeId', 'seqNumber', 'time']
     index = range(0)
     df = pd.DataFrame(index=index, columns=columns)
-    return df
-
-def scan_to_dataframe(df):
     all_items = scan_items_AWS()['Items']
     for i in range(len(all_items)):
         item_dict = {'deviceId': all_items[i]['deviceId'], 'timestamp': datetime.fromtimestamp(int(all_items[i]['timestamp'])),
@@ -175,6 +72,7 @@ def scan_to_dataframe(df):
         df.loc[i] = item_dict
     return df
 
+# Call this:
 def last_timestamp(df):
     num_items = len(df.index)
     str = datetime.strftime(df.timestamp[num_items - 1], "%a %b %d %H:%M:%S %Y")
@@ -182,50 +80,30 @@ def last_timestamp(df):
     timestamp = datetime.timestamp(dt)
     return(round(timestamp))
 
+# Call this:
 def append_to_dataframe(df, new_items):
 
-    prev_num_items = len(df.index)
+    num_items = len(df.index)
 
     i = 0
     for new_item in new_items:
         item_dict = {'deviceId': new_item['deviceId'], 'timestamp': datetime.fromtimestamp(int(new_item['timestamp'])),
                     'data': new_item['payload']['data'], 'deviceTypeId': new_item['payload']['deviceTypeId'],
                     'seqNumber': new_item['payload']['seqNumber'], 'time': new_item['payload']['time']}
-        df.loc[i+prev_num_items] = item_dict
+        df.loc[i+num_items] = item_dict
         i += 1
 
-    num_items = len(df.index)
-
-    return df, num_items
-
-# def plot_items(df):
-#     fig = px.line(df, x=df.timestamp, y=df.data, title='Sigfox Data')
-#     return fig
+    return df
 
 #----------------------------------------------------------------------------------------------------------------------#
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 demo = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-tableName = 'sigfox_demo'
-num_items = 30
-online = 0
-reset = 1
-flag = 1
+file = open("config.txt", 'r')
+tableName = file.readline().rstrip()
+online = int(file.readline())
 
 #----------------------------------------------------------------------------------------------------------------------#
-# Reinialize table if reset is True
-if reset:
-    # Delete the previous table
-    delete_sigfox_table_AWS()
-    print("Previous sigfox table deleted.")
-
-    # Create the table
-    sigfox_table = create_sigfox_table_AWS()
-    print("Creating a new sigfox table")
-    print("Table status: ", sigfox_table.table_status)
-
-    # Fill the table
-    populate_table()
 
 demo.layout = html.Div(
     html.Div([
@@ -239,28 +117,11 @@ demo.layout = html.Div(
     ])
 )
 
-# demo.run_server(dev_tools_hot_reload=False)
-
 @demo.callback(Output('sigfox-demo', 'figure'), Input('interval-component', 'n_intervals'))
 def update_graph_live(n):
 
-    # Create a DataFrame
-    df_empty = create_dataframe()
-
-    # Scan the table to the DataFrame
-    df_sigfox = scan_to_dataframe(df_empty)
-
-    # # Set the deviceId and timestamp of the most recent item
-    # deviceId = '12CAC94'
-    # timestamp = last_timestamp(df_sigfox)
-    #
-    # # Get all items that are newer than the most recent known item
-    # new_items = query_and_project_items_AWS(deviceId, timestamp)
-
-    # # Add the new items to the dataframe
-    # if len(new_items):
-    #     df_sigfox, num_items = append_to_dataframe(df_sigfox, new_items)
-    # new_items.clear()
+    # Scan the table to a DataFrame
+    df_sigfox = scan_to_dataframe()
 
     data = {
         'time': [],
@@ -284,35 +145,6 @@ def update_graph_live(n):
     }, 1, 1)
 
     return fig
-
-# @demo.callback(Output('sigfox-demo', 'figure'), Input('interval-component', 'n_intervals'))
-# def update_graph_live(n):
-#
-#     print(n)
-#     data = {
-#         'time': [],
-#         'Altitude': []
-#     }
-#
-#     for i in range(180):
-#         time = datetime.now() - timedelta(seconds=i*20)
-#         alt = random.random()
-#         data['Altitude'].append(alt)
-#         data['time'].append(time)
-#
-#     fig = make_subplots(rows=1, cols=1, vertical_spacing=0.2)
-#
-#     fig.append_trace({
-#         'x': data['time'],
-#         'y': data['Altitude'],
-#         'name': 'Altitude',
-#         'mode': 'lines+markers',
-#         'type': 'scatter'
-#     }, 1, 1)
-#
-#     return fig
-
-# os.system('python add.py')
 
 #----------------------------------------------------------------------------------------------------------------------#
 if __name__ == '__main__':
